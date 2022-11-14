8375b8ccc2a756765785b3d0b419c90ebc136d81
limhanyong <ninefirst77@gmail.com>
2022-11-14 14:36:52 +0900

    updated

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask, request
+import pandas as pd 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.sql import text 
+import json
 
-app = Flask(__name__)
+## DB 연결 Local
+def db_create():
+    # 로컬
+	# engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
+		
+	# Heroku
+    engine = create_engine("postgresql://shkegbfeivphah:183a9a9bcd13ff5522b75d240cca5335d21d0f905c1a181d15eddb6f24d39915@ec2-44-209-57-4.compute-1.amazonaws.com:5432/d4oja7hb5806v7", echo = False)
+
+    engine.connect()
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS iris(
+            sepal_length FLOAT NOT NULL,
+            sepal_width FLOAT NOT NULL,
+            pepal_length FLOAT NOT NULL,
+            pepal_width FLOAT NOT NULL,
+            species VARCHAR(100) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/iris.csv')
+    print(data)
+    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
+
+## 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+application = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world !"
-
-
+    db_create()
+    return "DB Created Done !!!!!!!!!!!!!!!"
 
 ## 카카오톡 텍스트형 응답
 @app.route('/api/sayHello', methods=['POST'])
@@ -31,6 +66,7 @@ def sayHello():
     }
 
     return responseBody
+
 
 ## 카카오톡 이미지형 응답
 @app.route('/api/showHello', methods=['POST'])
@@ -55,22 +91,6 @@ def showHello():
 
     return responseBody
 
-
-## 메인 로직!! 
-def cals(opt_operator, number01, number02):
-    if opt_operator == "addition":
-        return number01 + number02
-    elif opt_operator == "subtraction": 
-        return number01 - number02
-    elif opt_operator == "multiplication":
-        return number01 * number02
-    elif opt_operator == "division":
-        return number01 / number02
-
-
-import json
-
-
 ## 카카오톡 Calculator 계산기 응답
 @app.route('/api/calCulator', methods=['POST'])
 def calCulator():
@@ -78,7 +98,11 @@ def calCulator():
     print(body)
     params_df = body['action']['params']
     print(type(params_df))
+
+    print('-----')
     opt_operator = params_df['operators']
+    print('operator:', opt_operator)
+    print('-----')
     number01 = json.loads(params_df['sys_number01'])['amount']
     number02 = json.loads(params_df['sys_number02'])['amount']
 
@@ -101,8 +125,43 @@ def calCulator():
 
     return responseBody
 
+## Query 조회
+@app.route('/api/querySQL', methods=['POST'])
+def querySQL():
+    
+    body = request.get_json()
+    params_df = body['action']['params']
+    sepal_length_num = str(json.loads(params_df['sepal_length_num'])['amount'])
 
-# 1단계 : 코드수정    
-# 2단계 : 스킬 등록
-# 3단계 : 시나리오 등록한 스킬 호출
-# 4단계 : 배포
+    print(sepal_length_num, type(sepal_length_num))
+    query_str = f'''
+        SELECT sepal_length, species FROM iris where sepal_length >= {sepal_length_num}
+    '''
+
+    engine = create_engine("postgresql://shkegbfeivphah:183a9a9bcd13ff5522b75d240cca5335d21d0f905c1a181d15eddb6f24d39915@ec2-44-209-57-4.compute-1.amazonaws.com:5432/d4oja7hb5806v7", echo = False)
+
+    with engine.connect() as conn:
+        query = conn.execute(text(query_str))
+
+    df = pd.DataFrame(query.fetchall())
+    nrow_num = str(len(df.index))
+    answer_text = nrow_num
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text + "개 입니다."
+                    }
+                }
+            ]
+        }
+    }
+    return responseBody
+
+
+if __name__ == "__main__":
+    db_create()
+    app.run()
